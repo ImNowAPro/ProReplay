c9794973e8b741f897a5389f47aa457b437dcaf8
natekfl <nkutzan@gmail.com>
2020-06-13 12:46:40 -0400

    Added VarInt

--- src/main/java/me/imnowapro/proreplay/replay/PacketData.java
+++ src/main/java/me/imnowapro/proreplay/replay/PacketData.java
@@ -1,37 +1,45 @@
 package me.imnowapro.proreplay.replay;
 
 import com.comphenix.protocol.injector.netty.WirePacket;
+import com.google.common.base.Preconditions;
+import me.imnowapro.proreplay.replay.recording.converter.PacketUtil;
+
 import java.io.Serializable;
 import java.nio.ByteBuffer;
 
 public class PacketData implements Serializable {
 
-  private final ByteBuffer data;
+    private final ByteBuffer data;
 
-  public PacketData(int time, WirePacket packet) {
-    this.data = ByteBuffer.allocate(Integer.BYTES * 3 + packet.getBytes().length)
-        .putInt(time)
-        .putInt(Integer.BYTES + packet.getBytes().length)
-        .putInt(packet.getId())
-        .put(packet.getBytes());
-  }
+    public PacketData(int time, WirePacket packet) {
+        byte[] packetData = ByteBuffer.allocate(PacketUtil.varIntSize(packet.getId()) + packet.getBytes().length)
+                .put(PacketUtil.toVarInt(packet.getId()))
+                .put(packet.getBytes())
+                .array();
+        this.data = ByteBuffer.allocate(Integer.BYTES * 2 + packetData.length)
+                .putInt(time)
+                .putInt(packetData.length)
+                .put(packetData);
+    }
 
-  public PacketData(int time, byte[] data) {
-    this.data = ByteBuffer.allocate(Integer.BYTES * 2 + data.length)
-        .putInt(time)
-        .putInt(data.length)
-        .put(data);
-  }
+    public PacketData(int time, byte[] data) {
+        this.data = ByteBuffer.allocate(Integer.BYTES * 2 + data.length)
+                .putInt(time)
+                .putInt(data.length)
+                .put(data);
+    }
 
-  public int getTime() {
-    return this.data.getInt(0);
-  }
+    public int getTime() {
+        return this.data.getInt(0);
+    }
 
-  public int getID() {
-    return this.data.getInt(4);
-  }
+    public int getID() {
+        return this.data.getInt(4);
+    }
 
-  public byte[] serialize() {
-    return this.data.array();
-  }
+    public byte[] serialize() {
+        return this.data.array();
+    }
+
+
 }
